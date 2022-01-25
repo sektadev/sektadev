326dc252c3d6e440b270a0045a92eab8355aa7f3
Roman Timashev <roman@tmshv.ru>
2022-01-25 18:03:42 +0300

    next init

---
+++ pages/index.tsx
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import { YMetrika } from '@/components/YMetrika'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+    const metrika = process.env.NEXT_PUBLIC_YANDEX_METRIKA
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>sekta.dev</title>
+                <link rel="icon" href="/favicon.ico" />
+
+                {!metrika ? null : (
+                    <YMetrika
+                        number={metrika}
+                        mode={'script'}
+                    />
+                )}
+            </Head>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    sekta.dev
+                </h1>
+            </main>
+
+            <footer className={styles.footer}>
+                <a
+                    href="https://unit4.io"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    design::unit
+                </a>
+            </footer>
+        </div>
+    )
+}
